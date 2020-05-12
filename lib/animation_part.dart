0949062ffa1486c640e4c347ec7860e86ae1e11d
hannah <huilee9999@gmail.com>
2020-05-12 10:09:33 -0400

    parse kml file

--- lib/animation_part.dart
+++ lib/animation_part.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_app/animation_explaination.dart';
+import 'package:flutter_app/kml/kml_demo_page.dart';
 import 'package:flutter_app/path_line_animation.dart';
 import 'package:flutter_app/stagger_animation_page.dart';
 import 'package:flutter_app/syntonic_logo_animation2.dart';
@@ -116,11 +117,20 @@ class AnimationPart extends StatelessWidget {
                 ),
 
                 GestureDetector(
-                  child: Text("Animation Explaination Session",
+                  child: Text("Animation Sharing",
                       style: TextStyle(fontSize: 30, color: Colors.yellow)),
                   onTap: () {
                     Navigator.push(
                         context, SlideRightRoute(page: AnimationSharing()));
+                  },
+                ),
+
+                GestureDetector(
+                  child: Text("KML Screen",
+                      style: TextStyle(fontSize: 30, color: Colors.pinkAccent)),
+                  onTap: () {
+                    Navigator.push(
+                        context, SlideRightRoute(page: KmlDemoPage()));
                   },
                 ),
 
